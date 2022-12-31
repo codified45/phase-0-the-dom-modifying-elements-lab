77818cef2d118dd977d25d3f7d8c4d5642d7c500
Jack Lach <jack45k@gmail.com>
2022-12-30 23:15:52 -0500

    commit tree 1670abe19158d2b4f64bbf09791468c74667efb3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// for (let i = 0; i < 3; i++) { const li = document.createElement('li'); li.textContent = (i + 1).toString(); ul.append(li)
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Jack is the champion";
+document.body.append(newHeader);
+
