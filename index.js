e229ffebdf2f1d424179156f1ac32d69cab992b2
Jack Lach <jack.lach4@gmail.com>
2023-02-05 05:10:20 -0500

    all 4 tests passing - also switched append() with prepend() so node would load before script executes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// selecting #main via query selector
+const mainElementByQuery = document.querySelector("#main");
+mainElementByQuery.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Jack is the champion";
+document.body.prepend(newHeader);
+
